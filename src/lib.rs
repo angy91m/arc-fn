df165681009c72100484672b61826c2a42c4d7b7
angy91m <angy91m@hotmail.it>
2024-01-07 02:52:33 +0100

    Fixed macro rules

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use std::sync::{Arc,Mutex};
-use futures::future::BoxFuture;
+pub use std::sync::{Arc,Mutex};
+pub use futures::future::BoxFuture;
 pub extern crate futures;
 
 pub trait SyncFnMut<T,O>: FnMut(T) -> O + Send + Sync  + 'static {}
@@ -64,66 +64,8 @@ impl<T,O> ArcAsyncFn<T,O> {
     }
 }
 
-#[macro_export]
-macro_rules! sync_fn {
-    ($cb:ident) => {
-        Box::new($cb)
-    };
-    ($cb:expr) => {
-        Box::new({$cb})
-    };
-    ($cb:expr) => {
-        Box::new({$cb})
-    };
-}
-#[macro_export]
-macro_rules! arc_sync_fn {
-    ($cb:ident) => {
-        $crate::ArcSyncFn::new($crate::sync_fn!($cb))
-    };
-    ($cb:expr) => {
-        $crate::ArcSyncFn::new($crate::sync_fn!($cb))
-    };
-    ($cb:expr) => {
-        $crate::ArcSyncFn::new($crate::sync_fn!($cb))
-    };
-}
-#[macro_export]
-macro_rules! async_fn {
-    ($cb:ident) => {
-        Box::new($cb)
-    };
-    (|$a:ident| $cb:tt) => {
-        Box::new(|$a|async move $cb.boxed())
-    };
-    (move |$a:ident| $cb:tt) => {
-        Box::new(|$a|async move $cb.boxed())
-    };
-    (|$a:tt| $cb:tt) => {
-        Box::new(|$a|async move $cb.boxed())
-    };
-    (move |$a:tt| $cb:tt) => {
-        Box::new(|$a|async move $cb.boxed())
-    };
-}
-#[macro_export]
-macro_rules! arc_async_fn {
-    ($cb:ident) => {
-        $crate::ArcAsyncFn::new( $crate::async_fn!($cb) )
-    };
-    (|$a:ident| $cb:tt) => {
-        $crate::ArcAsyncFn::new( $crate::async_fn!(|$a| $cb) )
-    };
-    (move |$a:ident| $cb:tt) => {
-        $crate::ArcAsyncFn::new( $crate::async_fn!(move |$a| $cb) )
-    };
-    (|$a:tt| $cb:tt) => {
-        $crate::ArcAsyncFn::new( $crate::async_fn!(|$a| $cb) )
-    };
-    (move |$a:tt| $cb:tt) => {
-        $crate::ArcAsyncFn::new( $crate::async_fn!(move |$a| $cb) )
-    };
-}
+#[macro_use]
+pub mod macros;
 
 
 #[cfg(test)]
